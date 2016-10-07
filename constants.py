49018dafd1c2f1334bae3bb60be7010faf17841e
dustin lab <yanshuoc@gmail.com>
2016-10-07 16:05:50 +0800

    init from MixClone preprocess

---
+++ constants.py
@@ -0,0 +1,65 @@
+'''
+Created on 2013-07-20
+
+@author: Yi Li
+
+pyloh.constants
+
+================================================================================
+
+Modified on 2014-04-05
+
+@author: Yi Li
+'''
+import numpy as np
+
+BAF_N_MIN = 0.49
+BAF_N_MAX = 0.51
+BAF_COUNTS_MIN = 10
+BAF_COUNTS_MAX = 95
+BAF_BINS = np.array(range(0, 100 + 1))/100.0
+LOH_FRAC_MAX = 0.25
+SITES_NUM_MIN = 20
+PHI_INIT = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+PHI_RANGE = [i/100.0 for i in range(2, 99)]
+BINOM_TEST_P = 0.5
+#default BINOM_TEST_THRED = 0.025
+# for 30x read depth,
+BINOM_TEST_THRED = 0.025
+
+BINOM_TEST_THRED_APM = 0.18
+
+APM_N_MIN = 0.4
+APM_N_MAX = 0.6
+
+
+HC_THRESH = 0.005
+
+BURN_IN = 10
+EPS = np.finfo(float).eps
+ETA = 1.0
+INF = float('inf')
+LL_RATIO_CHANGE_THRED = 0.01
+LL_PERCENT_CHANGE_THRED = 0.90
+
+TAU = 500
+SIGMA = 0.0001
+ERR = 0.01
+EMPIRI_BAF = 0.485
+EMPIRI_AAF = 1 - EMPIRI_BAF
+
+MU_N = EMPIRI_BAF/(EMPIRI_BAF + EMPIRI_AAF)
+COPY_NUMBER_NORMAL = 2
+COPY_NUMBER_BASELINE = 2
+ALLELE_TYPE_BASELINE = 'PM'
+
+UPDATE_WEIGHTS = {}
+UPDATE_WEIGHTS['x'] = 0.9
+UPDATE_WEIGHTS['y'] = 0.1
+
+
+
+CHROM_START = 0
+
+CHROM_IDX_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+              20, 21, 22]
---
+++ preprocess/utils.py
@@ -0,0 +1,314 @@
+'''
+Created on 2013-07-27
+
+@author: Yi Li
+
+pyloh.preprocess.utils
+
+================================================================================
+
+Modified on 2014-04-09
+
+@author: Yi Li
+'''
+import sys
+import numpy as np
+from scipy.stats import binom
+
+from mixclone import constants
+
+
+def BEDnParser(bed_file_name):
+    """TODO: Docstring for BEDnParser.
+    :returns: TODO
+
+    """
+    inbed = open(bed_file_name)
+
+    chroms = []
+    starts = []
+    ends = []
+    tumorReadsn = []
+    normalReads = []
+
+    for line in inbed:
+        fields = line.split('\t')
+        chrom_name = fields[0]
+        chrom_idx = chrom_name_to_idx(chrom_name)
+
+        if chrom_idx == -1:
+            continue
+
+        chrom_name, start, end, tumorrd, normalrd = fields[0:5]
+
+        chroms.append(chrom_name)
+        starts.append(int(start))
+        ends.append(int(end))
+        tumorReadsn.append(int(tumorrd))
+        normalReads.append(int(normalrd))
+
+    inbed.close()
+
+    return (chroms, starts, ends, tumorReadsn, normalReads)
+
+
+def BEDParser(bed_file_name):
+    inbed = open(bed_file_name)
+
+    chroms = []
+    starts = []
+    ends = []
+
+    for line in inbed:
+        fields = line.split('\t')
+        chrom_name = fields[0]
+        chrom_idx = chrom_name_to_idx(chrom_name)
+
+        if chrom_idx == -1:
+            continue
+
+        chrom_name, start, end = fields[0:3]
+
+        chroms.append(chrom_name)
+        starts.append(int(start))
+        ends.append(int(end))
+
+    inbed.close()
+
+    return (chroms, starts, ends)
+
+
+def chrom_idx_to_name(idx, format):
+    if format == 'UCSC':
+        chrom_name = 'chr' + str(idx)
+    elif format == 'ENSEMBL':
+        chrom_name = str(idx)
+    else:
+        print 'Error: %s not supported' % (format)
+        sys.exit(1)
+
+    return chrom_name
+
+
+def chrom_name_to_idx(chrom_name):
+    idx = -1
+
+    try:
+        idx = int(chrom_name.strip('chr'))
+    except:
+        pass
+
+    return idx
+
+
+def get_chrom_format(chroms):
+    format = 'NONE'
+
+    for chrom in chroms:
+        if chrom[0:3] == 'chr':
+            format = 'UCSC'
+            break
+        else:
+            try:
+                idx = int(chrom)
+                format = 'ENSEMBL'
+                break
+            except:
+                pass
+
+    if format == 'NONE':
+        print 'Error: %s not supported' % (chrom)
+        sys.exit(-1)
+    else:
+        return format
+
+
+def get_chrom_lens_idxs(chrom_idx_list, sam_SQ):
+    chrom_lens = []
+    chrom_idxs = []
+    for i in range(0, len(chrom_idx_list)):
+        chrom_idx = chrom_idx_list[i]
+
+        for j in range(0, len(sam_SQ)):
+            if chrom_idx == chrom_name_to_idx(sam_SQ[j]['SN']):
+                chrom_lens.append(int(sam_SQ[j]['LN']))
+                chrom_idxs.append(chrom_idx)
+                break
+
+    return (chrom_lens, chrom_idxs)
+
+
+def get_segment_name(chrom_name, start, end):
+
+    return '_'.join([chrom_name, 'start', str(start), 'end', str(end)])
+
+
+def normal_heterozygous_filter(counts):
+    BAF_N_MAX = constants.BAF_N_MAX
+    BAF_N_MIN = constants.BAF_N_MIN
+
+    I = counts.shape[0]
+    idx_keep = []
+
+    for i in xrange(0, I):
+        a_N = counts[i, 0]*1.0
+        b_N = counts[i, 1]*1.0
+        d_N = a_N + b_N
+        BAF_N = b_N/d_N
+
+        if BAF_N >= BAF_N_MIN and BAF_N <= BAF_N_MAX:
+            idx_keep.append(i)
+
+    counts = counts[idx_keep]
+
+    return counts
+
+
+def get_BAF_counts(counts):
+    BAF_bins = constants.BAF_BINS
+
+    a_N = counts[:, 0]*1.0
+    b_N = counts[:, 1]*1.0
+    a_T = counts[:, 2]*1.0
+    b_T = counts[:, 3]*1.0
+
+    BAF_N = b_N/(a_N + b_N)
+    BAF_T = b_T/(a_T + b_T)
+
+    BAF_counts, _, _ = np.histogram2d(BAF_N, BAF_T, bins=(BAF_bins, BAF_bins))
+
+    return BAF_counts
+
+def get_APM_frac_MAXMIN(counts):
+    """get the baf position that are average in the tumor bam
+
+    :counts: TODO
+    :returns: TODO
+
+    """
+
+    I = counts.shape[0]
+
+    sites_num_min = constants.SITES_NUM_MIN
+
+    APM_N_MIN = constants.APM_N_MIN
+    APM_N_MAX = constants.APM_N_MAX
+
+    if I < sites_num_min:
+        APM_frac = -1
+
+        return APM_frac
+
+    a_T = counts[:, 2]
+    b_T = counts[:, 3]
+    d_T = a_T + b_T
+    l_T = np.min(counts[:, 2:4], axis=1)
+    p_T = l_T * 1.0 / d_T
+
+    APM_num = np.where(np.logical_and(p_T > APM_N_MIN, p_T < APM_N_MAX))[0].shape[0]
+    APM_frac = APM_num*1.0/I
+
+    return APM_frac
+
+    pass
+
+def get_LOH_frac(counts):
+    I = counts.shape[0]
+
+    sites_num_min = constants.SITES_NUM_MIN
+    p = constants.BINOM_TEST_P
+    thred = constants.BINOM_TEST_THRED
+
+    if I < sites_num_min:
+        LOH_frac = -1
+
+        return LOH_frac
+
+    a_T = counts[:, 2]
+    b_T = counts[:, 3]
+    d_T = a_T + b_T
+    l_T = np.min(counts[:, 2:4], axis=1)
+    p_T = binom.cdf(l_T, d_T, p)
+
+    LOH_num = np.where(p_T < thred)[0].shape[0]
+    LOH_frac = LOH_num*1.0/I
+
+    return LOH_frac
+
+def get_APM_frac(counts):
+    """get the baf position that are average in the tumor bam
+
+    :counts: TODO
+    :returns: TODO
+
+    """
+
+    I = counts.shape[0]
+
+    sites_num_min = constants.SITES_NUM_MIN
+    p = constants.BINOM_TEST_P
+    thred = constants.BINOM_TEST_THRED_APM
+
+    if I < sites_num_min:
+        APM_frac = -1
+
+        return APM_frac
+
+    a_T = counts[:, 2]
+    b_T = counts[:, 3]
+    d_T = a_T + b_T
+    l_T = np.min(counts[:, 2:4], axis=1)
+    p_T = binom.cdf(l_T, d_T, p)
+
+    APM_num = np.where(p_T > thred)[0].shape[0]
+    APM_frac = APM_num*1.0/I
+
+    return APM_frac
+
+    pass
+
+def get_LOH_status(LOH_frac, baseline_thred):
+    LOH_FRAC_MAX = constants.LOH_FRAC_MAX
+
+    if LOH_frac == -1:
+        LOH_status = 'NONE'
+    elif LOH_frac < baseline_thred:
+        LOH_status = 'FALSE'
+    elif LOH_frac >= baseline_thred and LOH_frac < LOH_FRAC_MAX:
+        LOH_status = 'UNCERTAIN'
+    elif LOH_frac >= LOH_FRAC_MAX:
+        LOH_status = 'TRUE'
+    else:
+        LOH_status = 'ERROR'
+
+    return LOH_status
+
+
+def get_APM_status(APM_frac, baseline_thred_APM):
+    if APM_frac > baseline_thred_APM:
+        APM_status = "TRUE"
+    else:
+        APM_status = "FALSE"
+
+    return APM_status
+
+def remove_outliers(X):
+    std_thred = 0.05
+
+    idx_keep = []
+
+    n = X.shape[0]
+
+    for i in range(0, n):
+        if np.abs(X[i] - X.mean()) <= X.std():
+            idx_keep.append(i)
+
+    if len(idx_keep) == 0 or len(idx_keep) == n:
+        return X
+
+    X = X[idx_keep]
+
+    if X.std() < std_thred:
+        return X
+    else:
+        return remove_outliers(X)
