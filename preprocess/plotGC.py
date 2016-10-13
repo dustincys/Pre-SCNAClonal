9fc87e01070e0537853672d121750c78f78b4fdd
dustin lab <yanshuoc@gmail.com>
2016-10-13 15:47:28 +0800

    add the plotGC function, Interactively get GC corrected

--- preprocess/plotGC.py
+++ preprocess/plotGC.py
@@ -17,6 +17,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, LassoSelector
 from matplotlib.path import Path
+from matplotlib.colors import colorConverter
 
 import numpy as np
 
@@ -93,34 +94,51 @@ class GCStripePlot():
         self.n = n
 
         # parameters for plot
-        self.m0  = 0
-        self.c0  = 0
+        self.m0 = 0
+        self.c0 = 0
         self.alpha0 = 0.02
         self.area0 = 10
 
         self.m = 0
         self.c = 0
+
+        self.m1 = 0
+        self.c1 = 0
+
         self.alpha = 1
         self.area = 10
 
+        self.x = None
+        self.y = None
 
-    def _selectSamples(self, x, y):
+        self.colorin = colorConverter.to_rgba('red', self.alpha0)
+        self.colorout = colorConverter.to_rgba('blue', self.alpha0)
+
+    def output(self):
         """
         :returns: TODO
 
         """
+        return self.x, self.y, self.m, self.c
 
-        subplot_kw = dict(
-            xlim=(
-                min(x), max(x)), ylim=(
-                min(y), max(y)), autoscale_on=False)
-        fig, ax = plt.subplots(subplot_kw=subplot_kw)
+    def _updateM(self, mt):
+        """
 
-        pts = ax.scatter(x, y, s=10)
-        selector = SelectFromCollection(ax, pts, alpha_other=0.1)
+        :m: TODO
+        :returns: TODO
 
-        plt.draw()
-        selector.disconnect()
+        """
+
+        self.m = np.tan(np.arctan(self.m1) + mt)
+
+    def _updateC(self, ct):
+        """
+
+        :ct: TODO
+        :returns: TODO
+
+        """
+        self.c = self.c1 + ct
 
     def plot(self):
         """plot the graph
@@ -130,22 +148,25 @@ class GCStripePlot():
         """
 
         sampledSegs = np.random.choice(self.segments, self.n)
-        x = np.array(map(lambda seg: seg.gc, sampledSegs))
-        y = np.array(map(lambda seg: np.log(seg.tumor_reads_num + 1) -
-                         np.log(seg.normal_reads_num + 1), sampledSegs))
+        x0 = np.array(map(lambda seg: seg.gc, sampledSegs))
+        y0 = np.array(map(lambda seg: np.log(seg.tumor_reads_num + 1) -
+                          np.log(seg.normal_reads_num + 1), sampledSegs))
+
+        self.x = x0
+        self.y = y0
 
         fig, ax = plt.subplots()
 
-        pts = ax.scatter(x, y, s=self.area0, alpha=self.alpha0)
+        pts = ax.scatter(x0, y0, s=self.area0, alpha=self.alpha0, color = "b")
         plt.subplots_adjust(left=0.25, bottom=0.35)
 
-        A = np.vstack([x, np.ones(len(x))]).T
-        self.m0, self.c0 = np.linalg.lstsq(A, y)[0]
-        fl, = ax.plot(x, self.m0*x + self.c0, 'r', label='Fitted line')
-        hl = ax.axhline(y = np.median(y), linewidth=1, color='black')
+        A = np.vstack([x0, np.ones(len(x0))]).T
+        self.m0, self.c0 = np.linalg.lstsq(A, y0)[0]
+        self.m1, self.c1 = self.m0, self.c0
 
-
-
+        xseq = np.arange(min(x0), max(x0), (max(x0) - min(x0))/100.0)
+        fl, = ax.plot(xseq, self.m0*xseq + self.c0, 'r', label='Fitted line')
+        hl = ax.axhline(y=np.median(y0), linewidth=1, color='black')
 
         axcolor = 'lightgoldenrodyellow'
         axm = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
@@ -153,8 +174,9 @@ class GCStripePlot():
         axalpha = plt.axes([0.25, 0.2, 0.65, 0.03], axisbg=axcolor)
         axarea = plt.axes([0.25, 0.25, 0.65, 0.03], axisbg=axcolor)
 
-        sm = Slider(axm, 'slope', -2, 2, valinit=self.m0)
-        sc = Slider(axc, 'interception', min(y), max(y), valinit=self.c0)
+        sm = Slider(axm, 'slope', -np.pi/4, np.pi/4, valinit=0)
+        sc = Slider( axc, 'interception', -(max(self.y) - min( self.y)) / 4,
+                    (max(self.y) - min( self.y)) / 4, valinit=0)
         salpha = Slider(axalpha, 'alpha', 0, 0.8, valinit=self.alpha0)
         sarea = Slider(axarea, 'area', 1, 50, valinit=self.area0)
 
@@ -163,8 +185,11 @@ class GCStripePlot():
             :returns: TODO
 
             """
-            self.m = sm.val
-            fl.set_ydata(self.m*x + self.c)
+            y_axis = np.median(self.y)
+            x_axis = (y_axis - self.c) / self.m
+            self._updateM(sm.val)
+            self.c =  y_axis - self.m * x_axis
+            fl.set_ydata(self.m*xseq + self.c)
             fig.canvas.draw_idle()
 
         def update_c(val):
@@ -172,8 +197,8 @@ class GCStripePlot():
             :returns: TODO
 
             """
-            self.c = sc.val
-            fl.set_ydata(self.m*x + self.c)
+            self._updateC(sc.val)
+            fl.set_ydata(self.m*xseq + self.c)
             fig.canvas.draw_idle()
 
         def update_alpha(val):
@@ -191,15 +216,41 @@ class GCStripePlot():
 
             """
             self.area = sarea.val
-            pts.set_sizes(np.ones(len(x)) * self.area)
+            pts.set_sizes(np.ones(len(self.x)) * self.area)
             fig.canvas.draw_idle()
-
 
         sm.on_changed(update_m)
         sc.on_changed(update_c)
         salpha.on_changed(update_alpha)
         sarea.on_changed(update_area)
 
+        select_col = lambda array, colNum: map(lambda x: x[colNum], array)
+
+        def onselect(verts):
+            path = Path(verts)
+            xys = pts.get_offsets()
+            ind = np.nonzero([path.contains_point(xy) for xy in xys])[0]
+            self.x = np.array(select_col(xys[ind], 0))
+            self.y = np.array(select_col(xys[ind], 1))
+
+            self.colorin = colorConverter.to_rgba('red', self.alpha0)
+            self.colorout = colorConverter.to_rgba('blue', self.alpha0)
+            facecolors = np.array([self.colorout for xy in xys])
+            facecolors[ind] = self.colorin
+            pts.set_facecolors(facecolors)
+
+            A = np.vstack([self.x, np.ones(len(self.x))]).T
+            self.m, self.c = np.linalg.lstsq(A, self.y)[0]
+            self.m1, self.c1 = self.m, self.c
+            fl.set_ydata(self.m*xseq + self.c)
+            hl.set_ydata(np.median(self.y))
+
+            sm.reset()
+            sc.reset()
+
+            fig.canvas.draw_idle()
+
+        lasso = LassoSelector(ax, onselect)
 
         resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
         button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
@@ -207,18 +258,26 @@ class GCStripePlot():
         def reset(event):
             sm.reset()
             sc.reset()
-            salpha.reset()
-            sarea.reset()
-            plt.ion()
+            self.colorout = colorConverter.to_rgba('blue', self.alpha0)
+            facecolors = np.array([self.colorout for y in y0])
+            pts.set_facecolors(facecolors)
+            self.y = y0
+            self.x = x0
+            A = np.vstack([self.x, np.ones(len(self.x))]).T
+            self.m, self.c = np.linalg.lstsq(A, self.y)[0]
+            self.m1, self.c1 = self.m, self.c
+            fl.set_ydata(self.m*xseq + self.c)
+            hl.set_ydata(np.median(self.y))
 
         button.on_clicked(reset)
 
+        resetax = plt.axes([0.5, 0.025, 0.1, 0.04])
+        button_exit = Button(resetax, 'Ok', color=axcolor, hovercolor='0.975')
 
-        def onselect(verts):
-            print "lasso selected!"
-            fig.canvas.draw_idle()
+        def ok_exit(event):
+            plt.close()
 
-        lasso = LassoSelector(ax, onselect)
+        button_exit.on_clicked(ok_exit)
 
         plt.show()
 
